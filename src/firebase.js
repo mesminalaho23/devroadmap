16aaa1cb9c5f5b9cf646addb264f3324e4b4c6e1
`Alaho <mesminalaho23@gmail.com>
2026-04-21 18:00:08 +0200

    deploiement firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKIscz1XmHLCFukqT1arm60uwql2htxlQ",
+  authDomain: "devroadmap-3897c.firebaseapp.com",
+  projectId: "devroadmap-3897c",
+  storageBucket: "devroadmap-3897c.firebasestorage.app",
+  messagingSenderId: "363898380087",
+  appId: "1:363898380087:web:fe439ed53cae390d2d04b2",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
